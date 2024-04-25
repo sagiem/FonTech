98320c0d105f0ad6b184d574c853e3e79603228f
Maksim Kashin <sagiems@gmail.com>
2024-04-25 12:34:11 +0600

    Добавил две сущности

---
+++ FonTech.Api/Controllers/ReportController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FonTech.Api.Controllers;
+
+public class ReportController : ControllerBase
+{
+    // GET
+    public IActionResult Index()
+    {
+        return null;
+    }
+}
---
+++ FonTech.Domain/Interfaces/IAuditable.cs
@@ -0,0 +1,9 @@
+﻿namespace FonTech.Domain.Interfaces;
+
+public interface IAuditable
+{
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public long CreatedBy { get; set; }
+    public long UpdatedBy { get; set; }
+}
